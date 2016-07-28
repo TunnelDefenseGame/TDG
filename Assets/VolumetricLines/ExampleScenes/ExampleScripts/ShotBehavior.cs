09b6ba719e220b079f1cb1ae1c8219e4be1c98ce
Nicolas Naugle <naugle92@ufl.edu>
2016-07-27 21:40:43 -0400

    Laser beam shoots in the correct direction. Tunnel, ship, and camera have round numbers for rotations and translations.

--- Assets/VolumetricLines/ExampleScenes/ExampleScripts/ShotBehavior.cs
+++ Assets/VolumetricLines/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class ShotBehavior : MonoBehaviour {
 
+	Vector3 front;
+	float xx;
+	float yy;
+	float zz;
+
 	// Use this for initialization
 	void Start () {
-	
+		xx = transform.forward.x;
+		yy = transform.forward.y;
+		zz = transform.forward.z;
+		front = new Vector3 (0, 0, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position += transform.forward * Time.deltaTime * 3f;
+		
+		transform.position += front * Time.deltaTime * 3f;
 		//transform.position.y += Time.deltaTime * 3f;
 	}
 }
