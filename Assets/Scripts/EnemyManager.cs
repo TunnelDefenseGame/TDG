fd9f44084736ff7dc0fc70aefb8d4c5b6bf1cf54
Nicolas Naugle <naugle92@ufl.edu>
2016-10-02 18:37:06 -0400

    Feature - Enemies now spawn faster over time, capping out at 1 per second

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -4,17 +4,28 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
 
+	//Should the enemies still be spawning or are we done here?
+	public bool SpawnerEnabled;
+
+	//interval between spawn times for enemies
+	public float SpawnInterval;
+
 	public GameObject enemy;
-	public float spawnTime = 1f;
+	public float spawnTime;
 	public Transform[] spawnPoints;
 
 	public static int score;
 	public Text scoreText;
 
+	private float timer;
+
 	// Use this for initialization
 	void Start () {
+		SpawnerEnabled = true;
 		score = 0;
-		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		//Start this function
+		StartCoroutine (Spawn ());
+		//InvokeRepeating ("Spawn", spawnTime, spawnTime);
 		setScoreText ();
 	}
 
@@ -24,13 +35,27 @@ public class EnemyManager : MonoBehaviour {
 	}
 
 
-	void Spawn () {
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-	
-		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	IEnumerator Spawn () {
+		
+
+
+		while (SpawnerEnabled) {
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+			timer += Time.deltaTime;
+			if (timer >= SpawnInterval) {
+				Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+			
+				yield return new WaitForSeconds(SpawnInterval);
+				timer = 0;
+				if (SpawnInterval > 1.0f) {
+					SpawnInterval -= Mathf.Sqrt (Time.deltaTime);
+				}
+			}
+		}
 	}
 
-
+	//Update the score of the game
 	void setScoreText () {
 		scoreText.text = "Score: " + score.ToString ();
 		//print (score);
