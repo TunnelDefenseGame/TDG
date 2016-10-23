efd2a8af5465ce2c240aec197b5a7bb1e3ec412d
Nicolas Naugle <naugle92@ufl.edu>
2016-10-23 17:51:46 -0400

    random background script created. More can be added if more backgrounds are desired

---
+++ Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundBehavior : MonoBehaviour {
+
+	public Texture2D[] textures;
+
+	// Use this for initialization
+	void Start () {
+		//select a random texture from the list of textures
+		int textureIndex = Random.Range (0, textures.Length);
+
+
+		GetComponent<Renderer>().material.mainTexture = textures[textureIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
